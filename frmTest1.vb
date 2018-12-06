676ea433956301e5d88f24bc774e68e51dbe608f
KS0405 <45250709+KS0405@users.noreply.github.com>
2018-12-06 13:00:11 +0700

    Add files via upload

---
+++ frmTest1.vb
@@ -0,0 +1,31 @@
+﻿Public Class frmTest1
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles lblTax.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'txtHour.Text
+        'txtPayRate.Text
+        lblTotal.Text = Val(txtHour.Text) * Val(txtPayRate.Text)
+        lblTax.Text = Val(lblTotal.Text) * 3 / 100
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCal2_Click(sender As Object, e As EventArgs) Handles btnCal2.Click
+        Dim hours As Integer
+        Dim payRate, total, tax As Double
+
+        hours = Val(txtHour.Text)
+        payRate = Val(txtPayRate.Text)
+
+        total = hours * payRate
+        tax = total * 0.03
+
+        lblTotal.Text = total
+        lblTax.Text = tax
+
+    End Sub
+End Class
